7b11f727b8d8c6c76109ee083ad8a11398bb75e5
ahabanova <106812238+ahabanova@users.noreply.github.com>
2022-06-04 11:04:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const priKliknuti = () => {
+    let navigace = document.querySelector('.navigace');
+    navigace.classList.toggle('otevreno');
+
+    let hamburger = document.querySelector('.hamburger');
+    hamburger.classList.toggle('otevreno-hamburger');
+}
